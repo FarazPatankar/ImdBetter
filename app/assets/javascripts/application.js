abf8a9fe846cdb0cb668fa5c44419fbac764895a
Faraz Patankar <farazpatankar@gmail.com>
2018-02-07 18:17:15 +0530

    add jquery + materialize component setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require materialize-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+  $('.modal').modal();
+  $('select').material_select();
+  $('.materialboxed').materialbox();
+});
