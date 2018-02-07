3bfd8c4006c98efda26bf7f04b43637384d694f4
Faraz Patankar <farazpatankar@gmail.com>
2018-02-07 18:17:28 +0530

    missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root "static_pages#home"
+
+  resources :movies
+  post "/movies/add-actor" => "movies#add_actor"
+  resources :actors
 end
