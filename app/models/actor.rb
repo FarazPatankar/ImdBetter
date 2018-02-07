d664b3a45f95d9aa8ed3fc56cfcfd371022c800f
Faraz Patankar <farazpatankar@gmail.com>
2018-02-07 18:19:06 +0530

    missing validations

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,4 +2,7 @@ class Actor < ApplicationRecord
   enum gender: [:male, :female]
   has_many :castings
   has_many :movies, through: :castings
+
+  validates :name, presence: true
+  validates :gender, presence: true
 end
